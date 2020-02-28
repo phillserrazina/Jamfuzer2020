a73589e4e309bd7cb4803d0951d379d2e927cbca
Phill Serrazina <fserrazina99@gmail.com>
2020-02-28 18:37:21 +0000

    Added Basic Visibility Mechanics

---
+++ Assets/Scripts/Objects/Section.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour
+{
+    // VARIABLES
+
+    [SerializeField] private Color[] myColors = null;
+    private int currentSelectionInt;
+    
+    private Renderer myRenderer;
+
+    public bool debug;
+
+    private bool unseen = true;
+    private bool isVisible { get { return myRenderer.IsVisibleFrom(Camera.main); } }
+
+    // EXECUTION FUNCTIONS
+
+    private void Start() {
+        myRenderer = GetComponent<Renderer>();
+        FindObjectOfType<Blink>().blinkEvent += OnBlink;
+
+        currentSelectionInt = 0;
+        myRenderer.material.color = myColors[0];
+    }
+
+    private void Update() {
+        if (isVisible) unseen = false;
+        if (!isVisible && !unseen) {
+            ChangeColor();
+            unseen = true;
+            
+        }
+    }
+
+    private void OnBecameInvisible() {
+        ChangeColor();
+    }
+
+    // METHODS
+
+    private void OnBlink() {
+        if (!isVisible) return;
+        ChangeColor();
+    }
+
+    private void ChangeColor() {
+        currentSelectionInt++;
+        if (currentSelectionInt >= myColors.Length) currentSelectionInt = 0;
+        myRenderer.material.color = myColors[currentSelectionInt];
+    }
+}
---
+++ Assets/Scripts/Player/Blink.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    public delegate void BlinkDelegate();
+    public BlinkDelegate blinkEvent;
+    
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            PlayerBlink();
+        }
+    }
+
+    private void PlayerBlink() {
+        if (blinkEvent != null) blinkEvent();
+    }
+}
