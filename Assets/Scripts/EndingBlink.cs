742d11b0d3b06363d1b78f568911f3e1efd688b8
Rensei <32361344+17068274@users.noreply.github.com>
2020-03-01 22:14:20 +0000

    started working on ending

---
+++ Assets/Scripts/EndingBlink.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingBlink : MonoBehaviour
+{
+    [SerializeField] GameObject _obstacles = null;
+    [SerializeField] Hatch _hatch = null;
+    int stage = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<Blink>().blinkEvent += OnBlink;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBlink()
+    {
+        if(stage == 0)
+        {
+            _obstacles.SetActive(false);
+            _hatch.Unlock();
+            stage++;
+        }
+        else if(stage == 1)
+        {
+
+        }
+        else if(stage == 2)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
