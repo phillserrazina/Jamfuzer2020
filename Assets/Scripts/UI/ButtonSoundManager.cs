2040253180d66ea604da3b8078dbb77b68377c73
Phill Serrazina <fserrazina99@gmail.com>
2020-03-01 14:38:07 +0000

    Added Menu Sounds

---
+++ Assets/Scripts/UI/ButtonSoundManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSoundManager : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+{
+    [SerializeField] private string onSelectSound = null;
+    private AudioManager audioManager;
+
+    private void Start() {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        audioManager.Play(onSelectSound, 2);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        audioManager.Play(onSelectSound, 2);
+    }
+}
