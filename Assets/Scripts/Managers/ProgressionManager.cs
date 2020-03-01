14cbb55793499b945320a215f5e2ecdd791b7724
Phill Serrazina <fserrazina99@gmail.com>
2020-03-01 15:28:05 +0000

    Removed Debug Buttons

--- Assets/Scripts/Managers/ProgressionManager.cs
+++ Assets/Scripts/Managers/ProgressionManager.cs
@@ -23,10 +23,6 @@ public class ProgressionManager : MonoBehaviour
             locked = true;
             FindObjectOfType<SubtitlesManager>().UnlockDescriptions();
         }
-
-        if (Input.GetKeyDown(KeyCode.Z)) {
-            NextMemory();
-        }
     }
 
     private void UnlockHatch() {
