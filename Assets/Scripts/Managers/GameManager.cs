da732e67e7e1932dd1bf4c4866fc2cff0c9ca67b
Phill Serrazina <fserrazina99@gmail.com>
2020-03-01 23:06:28 +0000

    Added Pause Menu

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,14 +3,49 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public bool paused { get; private set; }
+    private CameraEffects eye;
+
+    [SerializeField] private GameObject pauseMenuGui = null;
+
     private void Awake() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        paused = false;
     }
 
     private void Start() {
         var am = FindObjectOfType<AudioManager>();
+        eye = FindObjectOfType<CameraEffects>();
         am?.StopAllSound();
         am?.Play("Game Theme");
+    }
+
+    private void Update() {
+        Cursor.lockState = paused ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = paused;
+
+        if (Input.GetButtonDown("Pause")) {
+            Pause();
+        }
+    }
+
+    public void Pause() {
+        if (eye == null) eye = FindObjectOfType<CameraEffects>();
+        paused = !paused;
+
+        Time.timeScale = paused ? 0f : 1f;
+        if (paused) eye.blinkAnim.SetTrigger("CloseEye");
+        if (!paused) eye.blinkAnim.SetTrigger("OpenEye");
+
+        if (!paused) pauseMenuGui.SetActive(false); 
+    }
+
+    private void TriggerPauseGui() {
+        pauseMenuGui.SetActive(true);
+    }
+
+    public void QuitGame() {
+        SceneManager.LoadScene(0);
     }
 }
