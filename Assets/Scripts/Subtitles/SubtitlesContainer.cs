fd146ffacab0c635f336a44a2cbfa1e0254537a7
Rensei <32361344+17068274@users.noreply.github.com>
2020-03-01 00:26:17 +0000

    Subtitles

---
+++ Assets/Scripts/Subtitles/SubtitlesContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[CreateAssetMenu(fileName = "New SubtitlesContainer", menuName = "Subtitles Container")]
+[System.Serializable]
+public class SubtitlesContainer : ScriptableObject
+{
+    //public string[] senteces;
+    public SentenceSequence[] sequences;
+}
+
+[System.Serializable]
+public class SentenceSequence
+{
+    public string[] sentences;
+}
---
+++ Assets/Scripts/Subtitles/SubtitlesManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SubtitlesManager : MonoBehaviour
+{
+    [SerializeField] SubtitlesContainer subtitles = null;
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlaySubtitles());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlaySubtitles()
+    {
+        int randomSequenceIndex = Random.Range(0, subtitles.sequences.Length-1);
+        Debug.Log(randomSequenceIndex);
+        for (int i = 0; i < subtitles.sequences[randomSequenceIndex].sentences.Length; i++)
+        {
+            text.text = subtitles.sequences[randomSequenceIndex].sentences[i];
+            yield return new WaitForSeconds(4f);
+        }
+
+        text.text = " ";
+        yield return new WaitForSeconds(15f);
+
+        StartCoroutine(PlaySubtitles());
+    }
+}
